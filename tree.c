bbaf7f098a6ce4a52d3222ea2ded247ee7785022
Mickael Louvel <Mickaellouvel@hotmail.com>
2022-06-23 12:33:02 +0200

    refactored insert function

--- tree.c
+++ tree.c
@@ -21,12 +21,30 @@ extern Node * createNode(char value){
     }
     node -> left = NULL;
     node -> right = NULL;
+    node -> parent = NULL;
     node -> value = value;
     return node;
 }
 
 /**
- * @brief Insert a new node to an existing node
+ * @brief Get the root of a tree
+ * 
+ * @param tree 
+ * @return Node* 
+ */
+extern Node * getRoot(Node * tree){
+    if(tree == NULL){
+        fprintf(stderr, "%s", "null pointer error\n");
+        exit(EXIT_FAILURE);
+    }
+    if(tree -> parent != NULL){
+        return getRoot(tree -> parent);
+    }
+    return tree;
+}
+
+/**
+ * @brief Insert a new node to an existing node, by order according to decimal value in ASCII table
  * 
  * @param tree 
  * @param n 
@@ -37,17 +55,72 @@ extern Node * insert(Node * tree, Node * n){
         fprintf(stderr, "%s", "null pointer error\n");
         exit(EXIT_FAILURE);
     }
-    if(tree -> left == NULL){
-        tree -> left = n;
-        return n;
+    if((int) n -> value < (int) tree -> value){
+        if(tree -> left == NULL){
+            tree -> left = n;
+            tree -> left -> parent = tree;
+            return n;
+        }
+        return insert(tree -> left, n);
     }
     if(tree -> right == NULL){
-        tree -> right = n;
-        return n;
-    }
-    return insert(tree -> left, n);
+            tree -> right = n;
+            tree -> right -> parent = tree;
+            return n;
+        }
+        return insert(tree -> right, n);
+    return NULL;
 }
 
+/**
+ * @brief Search a node by its value in a tree
+ * 
+ * @param tree 
+ * @param value 
+ * @return Node* 
+ */
+extern Node * search(Node * tree, char value){
+    if(tree == NULL){
+        fprintf(stderr, "%s", "null pointer error\n");
+        exit(EXIT_FAILURE);
+    }
+    if(value == '\0'){
+        fprintf(stderr, "%s", "error empty value\n");
+        exit(EXIT_FAILURE);
+    }
+    Node * root = getRoot(tree);
+    if(value == root -> value){
+        return root;
+    }
+    if(value == tree -> value){
+        return tree;
+    }
+    if(tree -> left != NULL){
+        if(value == tree -> left -> value){
+            return tree -> left;
+        }
+        return search(tree -> left, value);
+    }
+    if(tree -> right != NULL){
+        if(value == tree -> right -> value){
+            return tree -> right;
+        }
+        return search(tree -> right, value);
+    }
+    if(tree -> parent != NULL){
+        if(value == tree -> parent -> value){
+            return tree -> parent;
+        }
+        return search(tree -> parent, value);
+    }
+    return tree;
+}
+
+/**
+ * @brief Display non-null tree
+ * 
+ * @param tree 
+ */
 extern void display(Node * tree){
     if(tree == NULL){
         fprintf(stderr, "%s", "null pointer error\n");
@@ -63,11 +136,12 @@ extern void display(Node * tree){
 }
 
 int main(){
-    Node * tree = createNode('A');
+    Node * tree = createNode('a');
     for(int i = 98; i < 123; i++){
         Node * n = createNode(i);
         insert(tree, n);
     }
     display(tree);
+    printf("found : %c\n", search(tree, 'd') -> value);
     return EXIT_SUCCESS;
 }
--- tree.h
+++ tree.h
@@ -4,12 +4,14 @@
 typedef struct Node Node;
 
 struct Node {
+    Node * parent;
     Node * left;
     Node * right;
     char value;
 };
 
 extern Node * createNode(char value);
+extern Node * getRoot(Node * tree);
 extern Node * insert(Node * tree, Node * n);
 extern Node * search(Node * tree, char value);
 extern Node * delete(Node * tree, char value);
